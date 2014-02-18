6caed7efdb30b8d5522edd030910c306d6519984
Linxiangyu <lxyweb@gmail.com>
2014-02-18 19:29:28 +0800

    move MyMongoid::Document to file lib/my_mongoid/document.rb

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -1,56 +1,12 @@
 require "my_mongoid/version"
-require 'active_support/all'
+require "my_mongoid/document"
 
 module MyMongoid
-  module Document
-
-    extend ActiveSupport::Concern
-
-    included do
-      MyMongoid.register_model(self)
-    end
-
-
-    def initialize(attrs = {})
-      raise ArgumentError, 'The argument  is not a Hash object' unless attrs.class == Hash 
-      
-      @attributes = attrs
-    end
-
-    def attributes 
-      @attributes
-    end
-
-    def read_attribute(name)
-      @attributes[name]
-    end
-
-    def write_attribute(name, value)
-      @attributes[name] = value
-    end
-
-    def new_record?
-      true
-    end
-
-
-
-    module ClassMethods
-      def is_mongoid_model?
-        true
-      end
-    end
-    
-  end
 
 
   def self.models
     @models ||= []
   end
-
-  
-
-
 
   def self.register_model(klass)
     models << klass
---
+++ lib/my_mongoid/document.rb
@@ -0,0 +1,43 @@
+require 'active_support/all'
+
+module MyMongoid
+  module Document
+
+    extend ActiveSupport::Concern
+
+    included do
+      MyMongoid.register_model(self)
+    end
+
+
+    def initialize(attrs = {})
+      raise ArgumentError, 'The argument  is not a Hash object' unless attrs.class == Hash 
+      
+      @attributes = attrs
+    end
+
+    def attributes 
+      @attributes
+    end
+
+    def read_attribute(name)
+      @attributes[name]
+    end
+
+    def write_attribute(name, value)
+      @attributes[name] = value
+    end
+
+    def new_record?
+      true
+    end
+
+
+    module ClassMethods
+      def is_mongoid_model?
+        true
+      end
+    end
+    
+  end
+end
