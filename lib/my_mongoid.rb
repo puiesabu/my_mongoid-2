8cf8fc947d43da992a8ef49601614115ac9f82ec
Linxiangyu <lxyweb@gmail.com>
2014-02-18 19:26:26 +0800

    Finish Model attributes and new

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -11,6 +11,29 @@ module MyMongoid
     end
 
 
+    def initialize(attrs = {})
+      raise ArgumentError, 'The argument  is not a Hash object' unless attrs.class == Hash 
+      
+      @attributes = attrs
+    end
+
+    def attributes 
+      @attributes
+    end
+
+    def read_attribute(name)
+      @attributes[name]
+    end
+
+    def write_attribute(name, value)
+      @attributes[name] = value
+    end
+
+    def new_record?
+      true
+    end
+
+
 
     module ClassMethods
       def is_mongoid_model?
