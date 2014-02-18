ad93b286ff0434a3d48521fc416bb9aab39c0d9d
Linxiangyu <lxyweb@gmail.com>
2014-02-18 19:10:36 +0800

    Finish Declare a model

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -1,5 +1,35 @@
 require "my_mongoid/version"
+require 'active_support/all'
 
 module MyMongoid
-  # Your code goes here...
+  module Document
+
+    extend ActiveSupport::Concern
+
+    included do
+      MyMongoid.register_model(self)
+    end
+
+
+
+    module ClassMethods
+      def is_mongoid_model?
+        true
+      end
+    end
+    
+  end
+
+
+  def self.models
+    @models ||= []
+  end
+
+  
+
+
+
+  def self.register_model(klass)
+    models << klass
+  end
 end
